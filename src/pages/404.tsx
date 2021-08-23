76a12ef215b6e919b2d2ae04f39451ea05e4765b
amakazor <amakazor@gmail.com>
2021-08-24 01:30:43 +0200

    Changed 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+            <p>Sooooooo.... how didyou get here?</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
