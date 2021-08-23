1374604e28c9f69bf6191db16ab0a265a985bc93
amakazor <amakazor@gmail.com>
2021-08-24 01:29:31 +0200

    Added typescript, styled-components and OpenSansFont

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
-    plugins: [],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                isTSX: true,
+                jsxPragma: `jsx`,
+                allExtensions: true,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-styled-components`,
+            options: {
+                displayName: true,
+            },
+        },
+    ],
 };
