b4176d6c20387443a85e7ab64234c8f23be6e844
amakazor <amakazor@gmail.com>
2021-08-24 01:31:56 +0200

    Created global style

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import Theme from './src/themes/theme';
+import GlobalStyle from './src/global/globalStyle';
 
 export const wrapRootElement = ({element}) => (
     <ThemeProvider theme={Theme}>
+        <GlobalStyle />
         {element}
     </ThemeProvider>
 );
---
+++ src/global/globalStyle.tsx
@@ -0,0 +1,34 @@
+import {createGlobalStyle} from 'styled-components';
+import '@fontsource/open-sans';
+import Theme from '../themes/theme';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html,
+    body,
+    #___gatsby,
+    #gatsby-focus-wrapper {
+        height: 100%;
+    }
+
+    body {
+        font-family: "Open Sans";
+        background: ${Theme.colors.background};
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyle;
