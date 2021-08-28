6e9e60b288b5c833e4c9c585c8ba8bb778d03439
amakazor <amakazor@gmail.com>
2021-08-28 13:10:41 +0200

    Sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+interface SidebarProps {
+    inactiveWidth: size;
+    activeWidth?: size;
+
+    active?: boolean;
+    widthTransition?: time;
+
+    backgroundColor?: string;
+
+    borderColor: string;
+    borderWidth: size;
+}
+
+const Sidebar = styled.aside<SidebarProps>`
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+
+    overflow: hidden;
+
+    width: calc(
+        ${props => (props.active ? props.activeWidth : props.inactiveWidth)} +
+            ${props => props.borderWidth}
+    );
+
+    transition: ${props => props.widthTransition && `width ${props.widthTransition}`};
+
+    border-right: ${props => props.borderWidth} solid ${props => props.borderColor};
+    background: ${props => props.backgroundColor};
+`;
+
+export default Sidebar;
