4d99c1ca23a129c97abd4e1476f6e2335d522f70
amakazor <amakazor@gmail.com>
2021-08-31 00:57:14 +0200

    Changed default H2 size

--- src/components/h2.tsx
+++ src/components/h2.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import HeadingBase, {HeadingBaseProps} from '../components/headingBase';
 
 export default function H2(props: HeadingBaseProps) {
-    return <HeadingBase as={'h2'} fontSize={'3.5rem'} {...props} />;
+    return <HeadingBase as={'h2'} fontSize={'calc(3rem + 1vw)'} {...props} />;
 }
