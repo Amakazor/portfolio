b1c2b8021ff207d33e2844771ca8e52cf26cbce4
amakazor <amakazor@gmail.com>
2021-09-22 22:20:39 +0200

    Added blank projects page

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {PageProps} from 'gatsby';
+import Layout from '../../components/layout';
+
+const IndexPage = (props: PageProps) => {
+    return <Layout pageProps={props}>TEST</Layout>;
+};
+
+export default IndexPage;
