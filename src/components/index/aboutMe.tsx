b48fa71971d1c0af5ad40a3598f8144598b5b136
amakazor <amakazor@gmail.com>
2021-09-22 22:19:47 +0200

    Changed aboutMe section a bit

--- src/components/index/aboutMe.tsx
+++ src/components/index/aboutMe.tsx
@@ -3,13 +3,9 @@ import Paragraph, {paragraphProps} from '../paragraph';
 import SidePaddedContainer from '../sidePaddedContainer';
 import UnderlinedLink from '../underlinedLink';
 import H2 from '../h2';
-import styled, {ThemeContext} from 'styled-components';
+import {ThemeContext} from 'styled-components';
 import MaterialIcon from '../materialIcon';
-
-const TextContainer = styled.div`
-    max-width: 80rem;
-    width: 95%;
-`;
+import MaxWidthContainer from '../maxWidthContainer';
 
 const AboutMe = () => {
     const theme = React.useContext(ThemeContext);
@@ -35,29 +31,29 @@ const AboutMe = () => {
                 </SidePaddedContainer>
                 <MaterialIcon fontSize={iconFontSize} iconName={'arrow_back_ios'} />
             </H2>
-            <TextContainer>
+            <MaxWidthContainer>
                 <Paragraph {...ParagraphProps} {...BoldProps}>
                     Hello There!
                 </Paragraph>
                 <Paragraph {...ParagraphProps}>
                     My name is Arkadiusz, I'm a developer focused mostly on webdevelopment
                     with commercial experience in both frontend and backend of websites.
-                    Besides webdevelopment I also write application and scripts in other
+                    Besides webdevelopment I also write applications and scripts in other
                     technologies like C-family languages, Python or Java in my spare time,
                     that are mostly focused in areas of data processing, procedural
                     generation and video games, the purpose of which is experimenting,
                     learning and just having fun doing something creative.
                 </Paragraph>
                 <Paragraph {...ParagraphProps}>
-                    I treat programming as my passion, as I love learning new things, like
-                    technologies, languages or some super-specific algorithms for an
-                    obscure problem that has been puzzling me for hours - that's the
-                    feeling i code for. That, and the ability to constanly improve my
-                    skills and becoming able to create even more things.
+                    I like to think about programming as my passion, as I love broadening
+                    my knowledge about technologies, languages or some super-specific
+                    algorithms for an obscure problem that has been puzzling me for hours
+                    - that's the feeling i code for. That, and the ability to constanly
+                    improve my skills and becoming able to create even more things.
                 </Paragraph>
                 <Paragraph {...ParagraphProps}>
                     When I'm not coding at the moment, you can find me doing some artsy
-                    stuff like 3D graphics, thinking about promising, new ideas like{' '}
+                    stuff like 3D graphics, thinking about promising new ideas like{' '}
                     <em>
                         <UnderlinedLink
                             href={'https://is7api.awrzawinski.xyz'}
@@ -85,7 +81,7 @@ const AboutMe = () => {
                     </em>
                     , playing video games or playing with my pets.
                 </Paragraph>
-            </TextContainer>
+            </MaxWidthContainer>
         </>
     );
 };
